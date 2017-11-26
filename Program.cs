66faa4ce9f15f22cc632fb2aa0ee4802c5983d3d
Acoustic6 <dorozhko.dmitry@gmail.com>
2017-11-26 14:04:54 +0300

    a thing to improve with yield

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Yield
+{
+    class Program
+    {
+        static Random ran = new Random();
+
+        static IEnumerable<int> GetRandomNumbers(int count)
+        {
+            List<int> ints = new List<int>();
+            for (int i = 0; i < count; i++)
+                ints.Add(ran.Next()); 
+            return ints;
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (var item in GetRandomNumbers(10))
+                Console.WriteLine(item);
+        }
+    }
+}
